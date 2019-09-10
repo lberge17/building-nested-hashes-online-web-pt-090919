6a02f2c9920d4ddd8ebf136f4ba6429045eca25f
Laura Berge <lauraeberge@gmail.com>
2019-09-10 00:52:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_firends => []
+      :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {},
